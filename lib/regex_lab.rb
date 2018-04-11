5aaf4ece62b0e8f11f830fac3b15f8f8818504c4
Heidi Hlavinka <hchlav@gmail.com>
2018-04-11 15:55:35 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -21,6 +21,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  number.scan(/\d{10}/) ? true : false
-  
+  phone.match(/\d{10}/) ? true : false
+
 end
