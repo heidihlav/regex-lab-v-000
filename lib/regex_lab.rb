2cedbaf095fd5a29e3cc019eb86b6027bc5f4fa7
Heidi Hlavinka <hchlav@gmail.com>
2018-04-11 15:54:47 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -21,7 +21,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.each do |number|
-    number.scan(/\d{10}/) ? true : false
-  end
+  number.scan(/\d{10}/) ? true : false
+  
 end
